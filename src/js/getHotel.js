3370a5308bdf7c5d8855de42ec56d2a7b5f223d9
Abd Dzuljalali Wal Ikram <aabedzul@gmail.com>
2020-08-17 21:02:46 +0800

    Added condition to hotel star

--- src/js/getHotel.js
+++ src/js/getHotel.js
@@ -2,6 +2,7 @@ const dataHotel = {
   nama_hotel: "Blue Corner Dive Resort",
   alamat_hotel:
     "Jalan Raya, Jungutbatu, Nusa Lembongan Island, Bali 80771, Indonesia",
+  star_hotel: 4,
   check_in: "14 June 2020",
   check_out: "21 June 2020",
   jumlah_tamu: 2,
@@ -13,30 +14,53 @@ const dataHotel = {
   jumlah_kamar: 7,
 };
 
+// menampilkan nama hotel
 document.getElementById("namaHotel").innerHTML = dataHotel.nama_hotel;
+
+// menampilkan bintang hotel sesuai value
+let bintang = dataHotel.star_hotel;
+for (let i = 0; i < bintang; i++) {
+  document.getElementById(
+    "starHotel"
+  ).innerHTML += `<i class="fas fa-star"></i>`;
+}
+
+// menampilkan alamat hotel
 document.getElementById("alamatHotel").innerHTML = dataHotel.alamat_hotel;
+
+// menampilkan tanggal check in & check out
 document.getElementById("checkinHotel").innerHTML = dataHotel.check_in;
 document.getElementById("checkoutHotel").innerHTML = dataHotel.check_out;
+
+// menampilkan jumlah tamu hotel
 document.getElementById("tamuHotel").innerHTML = dataHotel.jumlah_tamu;
+
+// menampilkan nama kamar
 document.getElementById("namaKamar").innerHTML = dataHotel.nama_kamar;
+
+// menampilkan dan convert harga kamar
 document.getElementById("hargaKamar").innerHTML = this.uang(
   dataHotel.harga_kamar
 );
+
+// menampilkan jumlah kamar yang dipesan
 document.getElementById("jumlahKamar").innerHTML = dataHotel.jumlah_kamar;
 
-// kondisi fasilitas kamar
+// menampilkan fasilitas kamar sesuai kondisi
 if (dataHotel.fasilitas_kamar.breakfast) {
-  document.getElementById("fasilitasKamar").innerHTML = "Breakfast";
+  document.getElementById("fasilitasKamar").innerHTML = "Bed & Breakfast";
+} else {
+  document.getElementById("fasilitasKamar").innerHTML = "Bed";
 }
 
-// menghitung total harga kamar
+// menghitung dan menampilkan total harga kamar
 let total = dataHotel.harga_kamar * dataHotel.jumlah_kamar;
 document.getElementById("totalHargaKamar").innerHTML = this.uang(total);
 
-// menghitung pajak kamar
+// menghitung dan menampilkan harga pajak kamar
 let tax = total * (10 / 100);
 document.getElementById("totalPajak").innerHTML = this.uang(tax);
 
-// menghitung total semua
+// menghitung dan menampilkan total semua yang harus dibayar
 let all = total + tax;
 document.getElementById("totalAll").innerHTML = this.uang(all);
